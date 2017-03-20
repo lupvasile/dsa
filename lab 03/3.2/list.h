7e36be806f1a447ea845c8348a0870958b615b4e
Vasi <vasile_lup2006@yahoo.com>
2017-03-20 19:04:40 +0200

    .

---
+++ lab 03/3.2/list.h
@@ -0,0 +1,33 @@
+#ifndef LIST_H_INCLUDED
+#define LIST_H_INCLUDED
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node
+{
+    char *key;
+    struct node *next;
+    struct node *prev;
+} NodeT;
+
+typedef struct
+{
+    int count;
+    NodeT *first;
+    NodeT *last;
+} ListT;
+
+int isEmpty(ListT *ptr);
+ListT *createList();
+NodeT *createNode(char *key);//returns pointer to new node
+//int insertAtFront(ListT *listPtr,NodeT *nodePtr);
+//int insertAtRear(ListT *listPtr,NodeT *nodePtr);
+void insertInOrder(ListT *ptrList, NodeT *nodePtr);
+void printList(FILE *f, ListT *ptrList, char separator);
+//NodeT *find(ListT *ptrList,int key);//returns pointer to node with given key or NULL if not found
+//NodeT *deleteByKey(ListT *ptrList,int key);//returns pointer to deleted node with given key or NULL if not found
+//NodeT *deleteFirst(ListT *ptrList);//returns pointer to first node or NULL if empty list
+
+
+#endif // LIST_H_INCLUDED
---
+++ lab 03/3.2/main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "list.h"
+
+FILE *openCheck(const char *name,const char *mode)
+{
+    FILE *f = fopen(name,mode);
+    if(f == NULL)
+    {
+        perror("");
+        exit(1);
+    }
+
+    return f;
+}
+
+
+int main(int argc,char* argv[])
+{
+    FILE *fin = openCheck(argv[1],"r");
+    FILE *fout = openCheck(argv[2],"w");
+
+    int bufLen = 1024;
+    //bufLen = getMaxLen(fin);
+
+    ListT *ptrList = createList();
+
+    char buf[bufLen];
+    while(fscanf(fin,"%s",buf) != EOF)
+                insertInOrder(ptrList,createNode(buf));
+
+    printList(fout,ptrList,'\n');
+
+    fclose(fin);
+    fclose(fout);
+    return 0;
+}
